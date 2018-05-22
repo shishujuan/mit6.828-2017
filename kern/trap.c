d2ebbacae2e9d1d7d537020b80c918e5651bbc95
ssj <ssjhust@163.com>
2018-05-22 15:16:01 +0000

    fix istrap to correct value(it did not affect the test result)

--- kern/trap.c
+++ kern/trap.c
@@ -83,24 +83,24 @@ trap_init(void)
 	void handler16();
 	void handler48();
 
-	SETGATE(idt[T_DIVIDE], 0, GD_KT, handler0, 0);
-	SETGATE(idt[T_DEBUG], 0, GD_KT, handler1, 0);
+	SETGATE(idt[T_DIVIDE], 1, GD_KT, handler0, 0);
+	SETGATE(idt[T_DEBUG], 1, GD_KT, handler1, 0);
 	SETGATE(idt[T_NMI], 0, GD_KT, handler2, 0);
 
 	// T_BRKPT DPL 3
-	SETGATE(idt[T_BRKPT], 0, GD_KT, handler3, 3);
+	SETGATE(idt[T_BRKPT], 1, GD_KT, handler3, 3);
 
-	SETGATE(idt[T_OFLOW], 0, GD_KT, handler4, 0);
-	SETGATE(idt[T_BOUND], 0, GD_KT, handler5, 0);
-	SETGATE(idt[T_ILLOP], 0, GD_KT, handler6, 0);
-	SETGATE(idt[T_DEVICE], 0, GD_KT, handler7, 0);
-	SETGATE(idt[T_DBLFLT], 0, GD_KT, handler8, 0);
-	SETGATE(idt[T_TSS], 0, GD_KT, handler10, 0);
-	SETGATE(idt[T_SEGNP], 0, GD_KT, handler11, 0);
-	SETGATE(idt[T_STACK], 0, GD_KT, handler12, 0);
-	SETGATE(idt[T_GPFLT], 0, GD_KT, handler13, 0);
-	SETGATE(idt[T_PGFLT], 0, GD_KT, handler14, 0);
-	SETGATE(idt[T_FPERR], 0, GD_KT, handler16, 0);
+	SETGATE(idt[T_OFLOW], 1, GD_KT, handler4, 0);
+	SETGATE(idt[T_BOUND], 1, GD_KT, handler5, 0);
+	SETGATE(idt[T_ILLOP], 1, GD_KT, handler6, 0);
+	SETGATE(idt[T_DEVICE], 1, GD_KT, handler7, 0);
+	SETGATE(idt[T_DBLFLT], 1, GD_KT, handler8, 0);
+	SETGATE(idt[T_TSS], 1, GD_KT, handler10, 0);
+	SETGATE(idt[T_SEGNP], 1, GD_KT, handler11, 0);
+	SETGATE(idt[T_STACK], 1, GD_KT, handler12, 0);
+	SETGATE(idt[T_GPFLT], 1, GD_KT, handler13, 0);
+	SETGATE(idt[T_PGFLT], 1, GD_KT, handler14, 0);
+	SETGATE(idt[T_FPERR], 1, GD_KT, handler16, 0);
 
 	// T_SYSCALL DPL 3
 	SETGATE(idt[T_SYSCALL], 0, GD_KT, handler48, 3);
