5a10ec8e287194eb453406ff494b067e11923bcf
ssj <ssjhust@163.com>
2018-06-23 14:46:55 +0000

    finish exercize 9.

--- kern/trap.c
+++ kern/trap.c
@@ -274,6 +274,15 @@ trap_dispatch(struct Trapframe *tf)
 
 	// Handle keyboard and serial interrupts.
 	// LAB 5: Your code here.
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_KBD) {
+		kbd_intr();
+		return;
+	}
+
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_SERIAL) {
+		serial_intr();
+		return;
+	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
