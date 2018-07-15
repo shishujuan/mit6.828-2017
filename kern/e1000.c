5ba24e11055c18b8ada2e68ebed5e7fe4d6ca994
ssj <ssjhust@163.com>
2018-07-15 03:22:06 +0000

    finish exercize 3.

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,11 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+int
+e1000_attachfn(struct pci_func *pcif)
+{
+	pci_func_enable(pcif);
+	cprintf("reg_base:%x, reg_size:%x\n", pcif->reg_base[0], pcif->reg_size[0]);
+	return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,11 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include "kern/pci.h"
+
+#define E1000_VENDER_ID_82540EM 0x8086
+#define E1000_DEV_ID_82540EM 0x100E
+
+int e1000_attachfn(struct pci_func *pcif);
+
 #endif	// JOS_KERN_E1000_H
