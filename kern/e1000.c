1f51a03b9da04c6630454bafee1a2424d53043f3
ssj <ssjhust@163.com>
2018-07-15 07:32:58 +0000

    finish exercize 4.

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,10 +2,19 @@
 #include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+volatile void *bar_va;
+#define E1000REG(offset) (void *)(bar_va + offset)
+
+
 int
 e1000_attachfn(struct pci_func *pcif)
 {
 	pci_func_enable(pcif);
 	cprintf("reg_base:%x, reg_size:%x\n", pcif->reg_base[0], pcif->reg_size[0]);
+
+	bar_va = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+
+	uint32_t *status_reg = (uint32_t *)E1000REG(E1000_STATUS);
+	assert(*status_reg == 0x80080783);
 	return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,6 +6,8 @@
 #define E1000_VENDER_ID_82540EM 0x8086
 #define E1000_DEV_ID_82540EM 0x100E
 
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+
 int e1000_attachfn(struct pci_func *pcif);
 
 #endif	// JOS_KERN_E1000_H
