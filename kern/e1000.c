4b22b55abfc433070d23826ee44e167cccf70902
ssj <ssjhust@163.com>
2018-07-15 07:48:32 +0000

    finish exercize 5.

--- kern/e1000.c
+++ kern/e1000.c
@@ -5,6 +5,10 @@
 volatile void *bar_va;
 #define E1000REG(offset) (void *)(bar_va + offset)
 
+struct e1000_tdh *tdh;
+struct e1000_tdt *tdt;
+struct e1000_tx_desc tx_desc_array[TXDESCS];
+char tx_buffer_array[TXDESCS][TX_PKT_SIZE];
 
 int
 e1000_attachfn(struct pci_func *pcif)
@@ -16,5 +20,44 @@ e1000_attachfn(struct pci_func *pcif)
 
 	uint32_t *status_reg = (uint32_t *)E1000REG(E1000_STATUS);
 	assert(*status_reg == 0x80080783);
+
+	e1000_transmit_init();
 	return 0;
+}
+
+static void
+e1000_transmit_init()
+{
+	int i;
+	for (i = 0; i < TXDESCS; i++) {
+		tx_desc_array[i].addr = PADDR(tx_buffer_array[i]);
+		tx_desc_array[i].cmd = 0;
+		tx_desc_array[i].status |= E1000_TXD_STAT_DD;
+	}
+
+	struct e1000_tdlen *tdlen = (struct e1000_tdlen *)E1000REG(E1000_TDLEN);
+	tdlen->len = TXDESCS;
+
+	uint32_t *tdbal = (uint32_t *)E1000REG(E1000_TDBAL);
+	*tdbal = PADDR(tx_desc_array);
+
+	uint32_t *tdbah = (uint32_t *)E1000REG(E1000_TDBAH);
+	*tdbah = 0;
+
+	tdh = (struct e1000_tdh *)E1000REG(E1000_TDH);
+	tdh->tdh = 0;
+
+	tdt = (struct e1000_tdt *)E1000REG(E1000_TDT);
+	tdt->tdt = 0;
+
+	struct e1000_tctl *tctl = (struct e1000_tctl *)E1000REG(E1000_TCTL);
+	tctl->en = 1;
+	tctl->psp = 1;
+	tctl->ct = 0x10;
+	tctl->cold = 0x40;
+
+	struct e1000_tipg *tipg = (struct e1000_tipg *)E1000REG(E1000_TIPG);
+	tipg->ipgt = 10;
+	tipg->ipgr1 = 4;
+	tipg->ipgr2 = 6;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,8 +6,73 @@
 #define E1000_VENDER_ID_82540EM 0x8086
 #define E1000_DEV_ID_82540EM 0x100E
 
+#define TXDESCS 32
+#define TX_PKT_SIZE 1518
+
 #define E1000_STATUS   0x00008  /* Device Status - RO */
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TXD_STAT_DD    0x00000001 /* Descriptor Done */
+#define E1000_TXD_CMD_EOP    0x00000001 /* End of Packet */
+#define E1000_TXD_CMD_RS     0x00000008 /* Report Status */
+
+
+/*transmit descriptor related*/
+struct e1000_tx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+}__attribute__((packed));
+
+struct e1000_tctl {
+	uint32_t rsv1:   1;
+	uint32_t en:     1;
+	uint32_t rsv2:   1;
+	uint32_t psp:    1;
+	uint32_t ct:     8;
+	uint32_t cold:   10;
+	uint32_t swxoff: 1;
+	uint32_t rsv3:   1;
+	uint32_t rtlc:   1;
+	uint32_t nrtu:   1;
+	uint32_t rsv4:   6;
+};
+
+struct e1000_tipg {
+	uint32_t ipgt:   10;
+	uint32_t ipgr1:  10;
+	uint32_t ipgr2:  10;
+	uint32_t rsv:    2;
+};
+
+struct e1000_tdt {
+	uint16_t tdt;
+	uint16_t rsv;
+};
+
+struct e1000_tdlen {
+	uint32_t zero: 7;
+	uint32_t len:  13;
+	uint32_t rsv:  12;
+};
+
+struct e1000_tdh {
+	uint16_t tdh;
+	uint16_t rsv;
+};
+
 
 int e1000_attachfn(struct pci_func *pcif);
+static void e1000_transmit_init();
 
 #endif	// JOS_KERN_E1000_H
