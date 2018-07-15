2e8935a746804c3f9eae4d4981a3a35ad4566e9f
ssj <ssjhust@163.com>
2018-07-15 08:54:12 +0000

    finish exercize 10.

--- kern/e1000.c
+++ kern/e1000.c
@@ -11,6 +11,12 @@ struct e1000_tdt *tdt;
 struct e1000_tx_desc tx_desc_array[TXDESCS];
 char tx_buffer_array[TXDESCS][TX_PKT_SIZE];
 
+struct e1000_rdh *rdh;
+struct e1000_rdt *rdt;
+struct e1000_rx_desc rx_desc_array[RXDESCS];
+char rx_buffer_array[RXDESCS][RX_PKT_SIZE];
+
+uint32_t E1000_MAC[6] = {0x52, 0x54, 0x00, 0x12, 0x34, 0x56};
 int
 e1000_attachfn(struct pci_func *pcif)
 {
@@ -23,6 +29,7 @@ e1000_attachfn(struct pci_func *pcif)
 	assert(*status_reg == 0x80080783);
 
 	e1000_transmit_init();
+	e1000_receive_init();
 
 	/*
 	 * transmit test
@@ -84,4 +91,53 @@ e1000_transmit(void *data, size_t len)
 	uint32_t next = (current + 1) % TXDESCS;
 	tdt->tdt = next;
 	return 0;
+}
+
+static void
+get_ra_address(uint32_t mac[], uint32_t *ral, uint32_t *rah)
+{
+	uint32_t low = 0, high = 0;
+	int i;
+
+	for (i = 0; i < 4; i++) {
+		low |= mac[i] << (8 * i);
+	}
+
+	for (i = 4; i < 6; i++) {
+		high |= mac[i] << (8 * i);
+	}
+
+	*ral = low;
+	*rah = high | E1000_RAH_AV;
+}
+
+static void
+e1000_receive_init()
+{
+	uint32_t *rdbal = (uint32_t *)E1000REG(E1000_RDBAL);
+	uint32_t *rdbah = (uint32_t *)E1000REG(E1000_RDBAH);
+	*rdbal = PADDR(rx_desc_array);
+	*rdbah = 0;
+
+	int i;
+	for (i = 0; i < RXDESCS; i++) {
+		rx_desc_array[i].addr = PADDR(rx_buffer_array[i]);
+	}
+
+	struct e1000_rdlen *rdlen = (struct e1000_rdlen *)E1000REG(E1000_RDLEN);
+	rdlen->len = RXDESCS;
+
+	rdh = (struct e1000_rdh *)E1000REG(E1000_RDH);
+	rdt = (struct e1000_rdt *)E1000REG(E1000_RDT);
+	rdh->rdh = 0;
+	rdt->rdt = RXDESCS-1;
+
+	uint32_t *rctl = (uint32_t *)E1000REG(E1000_RCTL);
+	*rctl = E1000_RCTL_EN | E1000_RCTL_BAM | E1000_RCTL_SECRC;
+
+	uint32_t *ra = (uint32_t *)E1000REG(E1000_RA);
+	uint32_t ral, rah;
+	get_ra_address(E1000_MAC, &ral, &rah);
+	ra[0] = ral;
+	ra[1] = rah;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -21,6 +21,23 @@
 #define E1000_TXD_CMD_EOP    0x00000001 /* End of Packet */
 #define E1000_TXD_CMD_RS     0x00000008 /* Report Status */
 
+
+#define RXDESCS 128
+#define RX_PKT_SIZE 1518
+#define E1000_RCTL 0x00100
+#define E1000_RCTL_EN     0x00000002    /* enable */
+#define E1000_RCTL_BAM    0x00008000    /* broadcast enable */
+#define E1000_RCTL_SECRC  0x04000000    /* Strip Ethernet CRC */
+#define E1000_RDBAL    0x02800  /* RX Descriptor Base Address Low - RW */
+#define E1000_RDBAH    0x02804  /* RX Descriptor Base Address High - RW */
+#define E1000_RDLEN    0x02808  /* RX Descriptor Length - RW */
+#define E1000_RDH      0x02810  /* RX Descriptor Head - RW */
+#define E1000_RDT      0x02818  /* RX Descriptor Tail - RW */
+#define E1000_RA       0x05400  /* Receive Address - RW Array */
+#define E1000_RAH_AV   0x80000000        /* Receive descriptor valid */
+#define E1000_RXD_STAT_DD       0x01    /* Descriptor Done */
+#define E1000_RXD_STAT_EOP      0x02    /* End of Packet */
+
 enum {
 	E_TRANSMIT_RETRY = 1,
 };
@@ -76,8 +93,37 @@ struct e1000_tdh {
 };
 
 
+/*receive descriptor related*/
+struct e1000_rx_desc {
+	uint64_t addr;
+	uint16_t length;
+	uint16_t chksum;
+	uint8_t status;
+	uint8_t errors;
+	uint16_t special;
+}__attribute__((packed));
+
+struct e1000_rdlen {
+	unsigned zero: 7;
+	unsigned len: 13;
+	unsigned rsv: 12;
+};
+
+struct e1000_rdh {
+	uint16_t rdh;
+	uint16_t rsv;
+};
+
+struct e1000_rdt {
+	uint16_t rdt;
+	uint16_t rsv;
+};
+
+
 int e1000_attachfn(struct pci_func *pcif);
 static void e1000_transmit_init();
 int e1000_transmit(void *data, size_t len);
+
+static void e1000_receive_init();
 
 #endif	// JOS_KERN_E1000_H
